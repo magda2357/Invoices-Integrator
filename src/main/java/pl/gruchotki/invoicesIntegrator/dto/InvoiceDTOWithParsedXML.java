f2f27c975d98cd4401dfe5ed6c19509ba5d877cd
MagdaSzut <magda.szut@gmail.com>
2023-02-08 22:24:09 +0100

    first attempt xsd schema mapping to objects

--- src/main/java/pl/gruchotki/invoicesIntegrator/dto/InvoiceDTOWithParsedXML.java
+++ src/main/java/pl/gruchotki/invoicesIntegrator/dto/InvoiceDTOWithParsedXML.java
@@ -1,10 +1,13 @@
 package pl.gruchotki.invoicesIntegrator.dto;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import pl.invoicesIntegrator.Faktury;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,7 +19,15 @@ import java.util.List;
 
 public class InvoiceDTOWithParsedXML {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        List<ItemDtoRequest> invoices = parseInvoicesXML();
+//        List<ItemDtoRequest> invoices = parseInvoicesXML();
+
+
+        File xmlFile = new File("src/main/resources/input.xml");
+
+        XmlMapper xmlMapper = (XmlMapper) new XmlMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        
+        Faktury fakturyType = xmlMapper.readValue(xmlFile, Faktury.class);
     }
 
     private static List<ItemDtoRequest> parseInvoicesXML() throws ParserConfigurationException, SAXException, IOException {
